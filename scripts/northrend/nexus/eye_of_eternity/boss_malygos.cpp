f5b75fd1d7609f67a35281e962c3a0e82c615a32
Tasssadar <vbocek@email.cz>
2010-01-08 18:57:33 +0100

    [vsd92] Malygos changes
    It is possible!
    Intro and vortex done now :)

--- scripts/northrend/nexus/eye_of_eternity/boss_malygos.cpp
+++ scripts/northrend/nexus/eye_of_eternity/boss_malygos.cpp
@@ -113,6 +113,7 @@ enum
     NPC_AOE_TRIGGER                = 22517,
     NPC_VORTEX                     = 30090,
     NPC_POWER_SPARK                = 30084,
+    NPC_SPARK_PORTAL               = 30118, // For power sparks
 
     //////////////// PHASE 2 ////////////////
     NPC_HOVER_DISC                 = 30248, // Maybe wrong, two following NPC flying on them (vehicle)
@@ -130,9 +131,20 @@ enum
     GO_ALEXSTRASZAS_GIFT           = 193905, //Loot chest
     GO_EXIT_PORTAL                 = 193908,
 
+    SAY_INTRO1                     = -1616000,
+    SAY_INTRO2                     = -1616001,
+    SAY_INTRO3                     = -1616002,
+    SAY_INTRO4                     = -1616003,
+    SAY_INTRO5                     = -1616004,
+    SAY_AGGRO1                     = -1616005,
+
     PHASE_NOSTART                  = 0,
-        SUBPHASE_FLY_DOWN          = 01,
+        SUBPHASE_FLY_UP            = 01,
+        SUBPHASE_UP                = 03,
+        SUBPHASE_FLY_DOWN1         = 04,
+        SUBPHASE_FLY_DOWN2         = 05,
     PHASE_FLOOR                    = 1,
+        SUBPHASE_VORTEX            = 11,
     PHASE_ADDS                     = 2,
     PHASE_DRAGONS                  = 3,
     PHASE_OUTRO                    = 4,
@@ -145,9 +157,25 @@ struct Locations
 };
 static Locations MalygosLoc[]=
 {
-	{754.346, 1300.87, 297.249},  //Center of platform
+    {754.346, 1300.87, 297.249},  //Center of platform
 
 };
+static Locations VortexLoc[]=
+{
+    {754, 1311, 330},
+    {701, 1288, 330},
+    {747, 1249, 325},
+    {795, 1291, 320},
+    {781, 1321, 315},
+    {752, 1329, 310},
+    {719, 1325, 305},
+    {720, 1297, 300},
+    {743, 1273, 295},
+    {764, 1290, 290},
+    {754, 1311, 287},
+};
+#define MAX_VORTEX              13
+
 #define FLOOR_Z                 266.17
 #define AIR_Z                   297.24   
 /*######
@@ -168,16 +196,32 @@ struct MANGOS_DLL_DECL boss_malygosAI : public ScriptedAI
     
     uint8 m_uiPhase; //Fight Phase
     uint8 m_uiSubPhase; //Subphase if needed
+    uint8 m_uiSpeechCount;
+    uint8 m_uiVortexPhase;
 
     uint32 m_uiEnrageTimer;
+    uint32 m_uiSpeechTimer[5];
+    uint32 m_uiTimer;
+    uint32 m_uiVortexTimer;
     
     void Reset()
     {
         m_uiPhase = PHASE_NOSTART;
         m_uiSubPhase = 0;
+        m_uiSpeechCount = 0;
+        m_uiVortexPhase = 0;
 
         m_uiEnrageTimer = 600000;
+        m_uiSpeechTimer[0] = 2000;
+        m_uiSpeechTimer[1] = 10000;
+        m_uiSpeechTimer[2] = 11000;
+        m_uiSpeechTimer[3] = 13000;
+        m_uiSpeechTimer[4] = 10000;
+        m_uiSpeechTimer[5] = 7000;
+        m_uiTimer = 7000;
+        m_uiVortexTimer = 10000;
         
+        m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
     }
 
     void JustReachedHome()
@@ -202,7 +246,7 @@ struct MANGOS_DLL_DECL boss_malygosAI : public ScriptedAI
 
     void Aggro(Unit* pWho)
     {
-        //DoScriptText(SAY_AGGRO, m_creature);
+        DoScriptText(SAY_AGGRO1, m_creature);
     }
 
     void JustDied(Unit* pKiller)
@@ -216,44 +260,127 @@ struct MANGOS_DLL_DECL boss_malygosAI : public ScriptedAI
      //   if (urand(0, 1))
        //     DoScriptText(SAY_KILL, m_creature);
     }
-    void LandOrLiftOff(bool land = true)
-    {
-        if(land == true)
-        {
-            m_creature->GetMap()->CreatureRelocation(m_creature, MalygosLoc[0].x, MalygosLoc[0].y, FLOOR_Z, m_creature->GetOrientation());
-            m_creature->SendMonsterMove(MalygosLoc[0].x, MalygosLoc[0].y, FLOOR_Z, m_creature->GetOrientation(), MONSTER_MOVE_TELEPORT, 0);
-            m_creature->HandleEmoteCommand(EMOTE_ONESHOT_LAND);
+    void DoMovement(float x, float y, float z, uint32 time, bool tofly = false)
+    {        
+        if(tofly){
+            m_creature->SetUInt32Value(UNIT_FIELD_BYTES_0, 50331648);
+            m_creature->SetUInt32Value(UNIT_FIELD_BYTES_1, 50331648);
+        }else{
+            m_creature->SetUInt32Value(UNIT_FIELD_BYTES_0, 0);
+            m_creature->SetUInt32Value(UNIT_FIELD_BYTES_1, 0);
         }
+        m_creature->GetMap()->CreatureRelocation(m_creature, x, y, z, m_creature->GetOrientation());
+        m_creature->SendMonsterMove(x, y, z, 0, m_creature->GetMonsterMoveFlags(), time);
     }
-    void MovementInform(uint32 uiType, uint32 uiPointId)
+    void DoVortex(uint8 phase)
     {
-        if(uiType != POINT_MOTION_TYPE)
+        if(phase == 0)
+        {
+            m_creature->GetMotionMaster()->Clear(false);
+            SetCombatMovement(false);
+            float x, y, z;
+            m_creature->GetPosition(x, y, z);
+            z = z + 40;
+            DoMovement(x, y, z, 3000, true);
+            if(Creature *pTrigger = GetClosestCreatureWithEntry(m_creature, NPC_AOE_TRIGGER, 60.0f))
+                pTrigger->CastSpell(pTrigger, SPELL_VORTEX_AOE_VISUAL, false);
+            
+        }
+        else if(phase == 1)
+        {
+            if(Creature *pTrigger = GetClosestCreatureWithEntry(m_creature, NPC_AOE_TRIGGER, 60.0f))
+                pTrigger->CastSpell(pTrigger, SPELL_VORTEX, false);    
+
+            Map* pMap = m_creature->GetMap();
+            if(!pMap)
                 return;
 
-        switch(uiPointId)
-        {
-            case 0:
-                //m_creature->RemoveMonsterMoveFlag(MONSTER_MOVE_SPLINE_FLY);
-                //m_creature->RemoveMonsterMoveFlag(MONSTER_MOVE_LEVITATING);
-                //LandOrLiftOff();
-                break;
+            Map::PlayerList const &lPlayers = pMap->GetPlayers();
+            for(Map::PlayerList::const_iterator itr = lPlayers.begin(); itr != lPlayers.end(); ++itr)
+            {
+                if(!itr->getSource()->isAlive())
+                    continue;
+                itr->getSource()->NearTeleportTo(VortexLoc[0].x, VortexLoc[0].y, VortexLoc[0].z, 0); 
+                itr->getSource()->CastSpell(itr->getSource(), SPELL_VORTEX, true);    
+            }        
         }
+        else if(phase > 1 && phase < 20){
+            Map* pMap = m_creature->GetMap();
+            if(!pMap)
+                return;
+            
+            
+
+            if(Creature *pVortex = m_creature->SummonCreature(NPC_VORTEX, VortexLoc[phase-1].x, VortexLoc[phase-1].y, VortexLoc[phase-1].z, 0, TEMPSUMMON_TIMED_DESPAWN, 10000))
+            {
+                pVortex->SetVisibility(VISIBILITY_OFF);
+                Map::PlayerList const &lPlayers = pMap->GetPlayers();
+                for(Map::PlayerList::const_iterator itr = lPlayers.begin(); itr != lPlayers.end(); ++itr)
+                {
+                    if(!itr->getSource()->isAlive())
+                        continue;
+
+                    
+                    float z = itr->getSource()->GetPositionZ() - VortexLoc[phase-1].z;
+                    itr->getSource()->KnockBackFrom(pVortex,-float(pVortex->GetDistance2d(itr->getSource())),-z);
+                    if(VortexLoc[phase-1].z == 287)
+                        itr->getSource()->NearTeleportTo(VortexLoc[10].x, VortexLoc[10].y, VortexLoc[10].z, 0);
+                }
+            }
+        }else if (phase == 20)
+        {    
+            m_creature->GetMotionMaster()->Clear(false);
+            SetCombatMovement(true);
+            float x, y, z;
+            m_creature->GetPosition(x, y, z);
+            z = z - 40;
+            DoMovement(x, y, z, 2000);
+
+            if(m_creature->getVictim())
+                m_creature->GetMotionMaster()->MoveChase(m_creature->getVictim());
+        }
+        
     }
     void UpdateAI(const uint32 uiDiff)
     {
         if(m_uiPhase == PHASE_NOSTART)
         {
-            if(m_uiSubPhase == SUBPHASE_FLY_DOWN){
+            if(m_uiSubPhase == SUBPHASE_FLY_UP){
                 float x, y, z;
                 m_creature->GetPosition(x, y, z);
-                m_creature->GetMap()->CreatureRelocation(m_creature, x, y, z + 20, m_creature->GetOrientation()); 
-                m_creature->SendMonsterMove(x, y, z + 40, 0, MONSTER_MOVE_FLY, 10);
-                m_creature->AddMonsterMoveFlag(MONSTER_MOVE_LEVITATING);
-                WorldPacket data;
-                m_creature->BuildHeartBeatMsg(&data);
-                m_creature->SendMessageToSet(&data, false);
-                m_uiSubPhase = 0;
+                z = z + 40;
+                DoMovement(x, y, z, 0, true);
+                m_uiSubPhase = SUBPHASE_UP;
             }
+            else if(m_uiSubPhase == SUBPHASE_UP){
+                if(m_uiSpeechTimer[m_uiSpeechCount] <= uiDiff)
+                {
+                    DoScriptText(SAY_INTRO1-m_uiSpeechCount, m_creature);
+                    m_uiSpeechCount++;
+                    if(m_uiSpeechCount == 5){
+                        m_uiSubPhase = SUBPHASE_FLY_DOWN1;
+                    }
+                }else m_uiSpeechTimer[m_uiSpeechCount] -= uiDiff;
+            }
+            else if(m_uiSubPhase == SUBPHASE_FLY_DOWN1){
+                if(m_uiTimer <= uiDiff)
+                {
+                    float x, y, z;
+                    m_creature->GetPosition(x, y, z);
+                    z = z - 40;
+                    DoMovement(x, y, z, 0, false);
+                    m_uiSubPhase = SUBPHASE_FLY_DOWN2;
+                    m_uiTimer = 1500;
+                }else m_uiTimer -= uiDiff;
+            }
+            else if(m_uiSubPhase == SUBPHASE_FLY_DOWN2){
+                if(m_uiTimer <= uiDiff)
+                {
+                    m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+                    m_uiSubPhase = 0;
+                    m_uiPhase = PHASE_FLOOR;
+                }else m_uiTimer -= uiDiff;
+            }    
         }
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
@@ -266,7 +393,42 @@ struct MANGOS_DLL_DECL boss_malygosAI : public ScriptedAI
         }else m_uiEnrageTimer -= uiDiff;
 
         if(m_uiPhase == PHASE_FLOOR)
-        {}
+        {
+            if(m_uiSubPhase == SUBPHASE_VORTEX)
+            {
+                if(m_uiTimer <= uiDiff)
+                {
+                    DoVortex(m_uiVortexPhase);
+                    
+                    if(m_uiVortexPhase == 1 || m_uiVortexPhase == 11){
+                        if(m_uiVortexPhase == 1)
+                            DoCast(m_creature, SPELL_VORTEX_DUMMY);
+                        m_uiTimer = 300;
+                    }else
+                        m_uiTimer = 1000;
+
+                    if(m_uiVortexPhase >= MAX_VORTEX)
+                    {
+                        m_uiSubPhase = 0;
+                        DoVortex(20);
+                        return;
+                    }
+                    m_uiVortexPhase++;
+                }else m_uiTimer -= uiDiff;
+                return;
+            }
+
+            if(m_uiVortexTimer <= uiDiff)
+            {
+                DoVortex(0);
+                m_uiVortexPhase = 1;
+                m_uiSubPhase = SUBPHASE_VORTEX;
+                m_uiVortexTimer = 50000;
+                m_uiTimer = 3000;
+            }else m_uiVortexTimer -= uiDiff;
+
+            DoMeleeAttackIfReady();
+        }
 
     }
 };
@@ -292,7 +454,7 @@ bool GOHello_go_focusing_iris(Player* pPlayer, GameObject* pGo)
 
     if(Creature *pMalygos = GetClosestCreatureWithEntry(pGo, NPC_MALYGOS, 150.0f))
     {
-        ((boss_malygosAI*)pMalygos->AI())->m_uiSubPhase = SUBPHASE_FLY_DOWN;
+        ((boss_malygosAI*)pMalygos->AI())->m_uiSubPhase = SUBPHASE_FLY_UP;
     }
     pGo->Delete();
     return false;
@@ -353,4 +515,35 @@ SELECT * FROM creature_template WHERE entry=28859;
 UPDATE `creature_template` SET `InhabitType` = '3' WHERE `entry` =28859 LIMIT 1 ;
 UPDATE `mangostest`.`creature` SET `spawndist` = '0',
 `MovementType` = '0' WHERE `creature`.`guid` =132313 LIMIT 1 ;
+
+INSERT INTO `scriptdev2`.`script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
+('-1616000', 'Lesser beings, intruding here! A shame that your excess courage does not compensate for your stupidity!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '14512', '1', '0', '457', NULL),
+('-1616001', 'None but the blue dragonflight are welcome here! Perhaps this is the work of Alexstrasza? Well then, she has sent you to your deaths.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '14513', '1', '0', '457', NULL),
+('-1616002', 'What could you hope to accomplish, to storm brazenly into my domain? To employ MAGIC? Against ME? <Laughs>', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '14514', '1', '0', '457', NULL),
+('-1616003', 'I am without limits here... the rules of your cherished reality do not apply... In this realm, I am in control...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '14515', '1', '0', '457', NULL),
+('-1616004', 'I give you one chance. Pledge fealty to me, and perhaps I won''t slaughter you for your insolence!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '14516', '1', '0', '457', NULL),
+('-1616005', 'My patience has reached its limit, I WILL BE RID OF YOU!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '14517', '1', '0', '1', NULL);
+
+UPDATE `mangostest`.`creature_template` SET `AIName` = 'NullAI' WHERE `creature_template`.`entry` =22517 LIMIT 1 ;
+
+UPDATE `mangostest`.`creature_template` SET `AIName` = 'NullAI' WHERE `creature_template`.`entry` =30090 LIMIT 1 ;
+
+UPDATE `mangostest`.`creature_template` SET `AIName` = 'NullAI' WHERE `creature_template`.`entry` =30118 LIMIT 1 ;
+
+UPDATE `mangostest`.`creature_template` SET `AIName` = 'NullAI' WHERE `creature_template`.`entry` =30334 LIMIT 1 ;
+
+UPDATE `mangostest`.`creature_template` SET `AIName` = 'NullAI' WHERE `creature_template`.`entry` =31253 LIMIT 1 ;
+
+UPDATE `mangostest`.`creature_template` SET `AIName` = 'NullAI' WHERE `creature_template`.`entry` =32448 LIMIT 1 ;
+
+
+UPDATE `mangostest`.`creature_template` SET `modelid_A` = '11686',
+`modelid_A2` = '11686',
+`modelid_H` = '11686',
+`modelid_H2` = '11686',
+`minhealth` = '44120',
+`maxhealth` = '44120',
+`AIName` = '',
+`flags_extra` = '2' WHERE `creature_template`.`entry` =22517 LIMIT 1 ;
+
 */
