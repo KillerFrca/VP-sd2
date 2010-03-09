82194575571f65362f5021e278793aa14b6fe559
KillerFrca <honzakf@seznam.cz>
2010-03-09 01:36:08 +0100

    Freya - header
    Signed-off-by: KillerFrca <honzakf@seznam.cz>

--- scripts/northrend/ulduar/ulduar/boss_freya.cpp
+++ scripts/northrend/ulduar/ulduar/boss_freya.cpp
@@ -27,7 +27,49 @@ EndScriptData */
 /*
 #define SAY_AGGRO -1
 #define SAY_SLAY -1
+
+
 */
+enum
+{
+    //general abilities
+    SPELL_SUNBEAM                       = 62623,
+    H_SPELL_SUNBEAM                     = 62872,
+    SPELL_ATTUNED_TO_NATURE             = 62519,
+    SPELL_TOUCH_OF_EONAR                = 62528,
+    H_SPELL_TOUCH_OF_EONAR              = 62892,
+    SPELL_NATURE_BOMB                   = 64587,
+    H_SPELL_NATURE_BOMB                 = 64650,
+
+    SPELL_LIFEBINDERS_GIFT_SUMMON       = 62869,
+    SPELL_SUMMON_ALLIES_OF_NATURE       = 62678,
+    SPELL_NATURE_BOMB_SUMMON            = 64606,
+
+    //abilities with Elder Brightleaf
+    SPELL_UNSTABLE_ENERGY               = 62451,
+    H_SPELL_UNSTABLE_ENERGY             = 62865,
+    SPELL_BRIGHTLEAFS_ESSENCE           = 62385,
+
+    //abilities with Elder Ironbranch
+    SPELL_IRON_ROOTS                    = 62283,
+    H_SPELL_IRON_ROOTS                  = 62930,
+    SPELL_IRONBRANCHS_ESSENCE           = 62387,
+    
+    SPELL_STRENGTHENED_IRON_ROOTS_SUMM  = 63601,
+    
+    NPC_STRENGTHENED_IRON_ROOTS         = 33168,
+
+    //abilities with Elder Stonebark 
+    SPELL_GROUND_TREMOR                 = 62437,
+    H_SPELL_GROUND_TREMOR               = 62859,
+    SPELL_STONEBARKS_ESSENCE            = 62386,
+
+    NPC_NATURE_BOMB                     = 34129,
+
+    NPC_ELDER_BRIGHTLEAF                = 32915,
+    NPC_ELDER_IRONBRANCH                = 32913,
+    NPC_ELDER_STONEBARK                 = 32914,
+};
 
 struct MANGOS_DLL_DECL boss_freyaAI : public ScriptedAI
 {
@@ -49,6 +91,14 @@ struct MANGOS_DLL_DECL boss_freyaAI : public ScriptedAI
 
     void JustDied(Unit *victim)
     {
+    }
+
+    bool IsCreatureAlive(Creature *pCreature)
+    {
+        if(GetClosestCreatureWithEntry(m_creature, pCreature, 150.0f)
+            return true;
+        
+        return false;
     }
 
     void Aggro(Unit* pWho)
